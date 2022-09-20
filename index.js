4963ff54bb541595dc73f61a4dcb8cc7e4a55f77
FelipeCarrascoH <fcarrasco@dhemax.cl>
2022-09-19 23:01:46 -0300

    first

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+
+
+app.get("/", (req, res) => {
+  
+    res.send('Holiwis');
+  
+});
+
+
+app.listen(3000, () => {
+  console.log(`App listening on port ${3000}`);
+});
